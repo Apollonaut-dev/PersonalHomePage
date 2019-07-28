870af9049979e0fed9609eb88c2437459b589570
Anthony de Vellis <anthonydevellis@gmail.com>
2019-07-27 21:33:04 -0400

    Added content, refactored routing and response handling

--- app.js
+++ app.js
@@ -13,22 +13,33 @@ module.exports = {
 		// attempt to find response on filesystem
 		// *this will need to be updated for SPA apis, query string-based calls don't return files necessarily
 		let route = router.route(path);
-		console.log(route.target);
-		fs.readFile( __dirname + route.target, null, function(error, data) {
+		let dir = __dirname;
+		let stat_code = 200;
+		console.log(route);
+		// check if route exists, else we have a 404 
+		if ( route === undefined ) {
+			route = router.route('/404');
+			stat_code = 404;
+		}
+		dir += route.target;
+		console.log("request: " + path);
+		console.log("resolution: " + dir);
+		fs.readFile( dir, null, function(error, data) {
 			if (error) {
+				console.log(error);
 				// TODO figure out how to find the difference
 				// between readFile errors and invalid requests
 				// fails on readFile should return code 500 not 404
 				// only requests for non existent pages or assets should return 404
 				// if error on looking for view file then respond 404 page view
-				response.writeHead(404);
-				fs.readFile(__dirname + router.route('/404.html').target, null, function(error, data) {
-					console.log(error);
+				stat_code = 500;
+				response.writeHead(stat_code);
+				fs.readFile(__dirname + router.route('/500').target, null, function(error, data) {
 					response.write(data);
 				});
 			} else {
 				// if we found the request, send data with appropriate type in header
-				response.writeHead(200, {'Content-Type': route.type});
+				response.writeHead(stat_code, {'Content-Type': route.type});
 				response.write(data);
 			}
 			response.end();
